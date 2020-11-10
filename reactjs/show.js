e78a9507b7afe42263c223c0e569021c44f50c21
Frida <55127994+johanssoncfrida@users.noreply.github.com>
2020-11-10 10:07:57 +0100

    finished 3.1

--- reactjs/show.js
+++ reactjs/show.js
@@ -2,7 +2,7 @@ function Show({hash, children}){
     const [route, setRoute]= React.useState(window.location.hash);
     React.useEffect(()=> 
       window.addEventListener("hashchange", 
-                 ()=> setRoute(route)), 
+                 ()=> setRoute({})), 
  []); 
     return hash===window.location.hash?children: false;
  } 
